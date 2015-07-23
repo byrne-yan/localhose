bc83ee3179a3c9034f79fadb2dbc4471dc6259b8
颜俊标 <byrne.yan@yahoo.com>
2015-07-23 11:58:24 +0800

    complete package description

--- package.js
+++ package.js
@@ -1,9 +1,7 @@
 Package.describe({
   name: 'vamp:localhose',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
+  summary: 'meteor package fork for https://github.com/jed/localhose.git',
   git: 'https://github.com/byrne-yan/localhose.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
